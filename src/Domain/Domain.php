fddcd5c12df5ecf4cc4bee6e378af7732466f2a3
Vitaliy <theyamshikov@yandex.ru>
2022-07-06 18:45:50 +0600

    upd

--- src/Domain/Domain.php
+++ src/Domain/Domain.php
@@ -2,7 +2,7 @@
 
 namespace ZnTool\RestClient\Domain;
 
-use ZnCore\Domain\Domain\Interfaces\DomainInterface;
+use ZnCore\Domain\Interfaces\DomainInterface;
 
 class Domain implements DomainInterface
 {
